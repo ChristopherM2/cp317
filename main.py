5577f86b0e9637026dd2751813370aff86c780c1
Chris M <math9620@mylaurier.ca>
2024-06-26 16:39:34 -0400

    update :3

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pymongo
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
-print("meow")
+
 mongo = os.getenv("MONGO")
-client = pymongo.MongoClient()
+client = pymongo.MongoClient(mongo)
 db = client["Home"]
 users = db["Users"]
 groups = db["Groups"]
 
-
 print(db.list_collection_names())
+
+user = {
+    "Name": "Christopher Matheson",
+    "ID": 1
+}
+users.insert_one(user)
