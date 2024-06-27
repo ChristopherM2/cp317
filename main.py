fb98d2cecd07709305c373d52e150f4f6f597f0c
Chris M <math9620@mylaurier.ca>
2024-06-27 10:10:40 -0400

    added functions

--- main.py
+++ main.py
@@ -12,11 +12,37 @@ groups = db["Groups"]
 
 print(db.list_collection_names())
 
-user = {
-    "Name": "Shayan Dhillon",
-    "ID": 2
-}
 
-group = {"Users": [1,2]}
-users.insert_one(user)
-groups.insert_one(group)
+def create_user(name):
+    id = users.count_documents({}) + 1
+    users.insert_one({
+        "name": name,
+        "id": id,
+    })
+    return id
+
+
+def create_group(name):
+    gid = groups.count_documents({}) + 1
+    groups.insert_one({
+        "name": name,
+        "users": [],
+        "gid": gid,
+    })
+    return gid
+
+
+def add_user_to_group(uid, gid):
+    user = users.find_one({"id": uid})
+    group = groups.find_one({"gid": gid})
+    if user is None or group is None:
+        return False
+    group["users"].append(uid)
+    groups.update_one({"gid": gid}, {"$set": group})
+    return True
+
+def get_user(uid):
+    return users.find_one({"id": uid})
+def get_group(gid):
+    return groups.find_one({"gid": gid})
+
